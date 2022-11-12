4d57eec8d26c6113d086fc2863c212bd4a6a2925
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-11-12 09:33:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img="";
+status1="";
+
 function preload()
 {
     img=loadImage("dog_cat.jpg");
@@ -8,7 +10,25 @@ function setup()
 {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector("cocossd","modelLoaded");
+    document.getElementById("status").innerHTML="status : detecting objects";
 }
+function modelLoaded()
+{
+  console.log("model loaded");
+  status1=true;
+  objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results)
+{
+  if(error)
+  {
+    console.log(error);
+  }
+  console.log(results);
+}
+
+
 
 function draw()
 {
@@ -18,4 +38,11 @@ function draw()
   noFill();
   stroke("#f705a7");
   rect(30,60,450,350);
+
+  fill("#fffb1f");
+  text("cat",320,120);
+  noFill();
+  stroke("#fffb1f");
+  rect(300,90,270,320);
+
 }
