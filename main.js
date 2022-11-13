4be395e214a9f7c1e24ab7075e3b41667b6290e5
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-11-13 09:59:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function setup()
 {
     canvas=createCanvas(640,420);
     canvas.center();
-    objectDetector=ml5.objectDetector("cocossd","modelLoaded");
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
     document.getElementById("status").innerHTML="status : detecting objects";
 }
 function modelLoaded()
